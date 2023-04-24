3961dba5d9346afb257cb7e783b78e30ffab4f94
Sami Turk <samiturk8@gmail.com>
2023-04-24 07:40:01 +0100

    added list

--- calculations.py
+++ calculations.py
@@ -1,4 +1,7 @@
 import csv
+list_average=[]
+list_high=[]
+list_low=[]
 
 with open('results.csv') as file:
     read = csv.reader(file, delimiter=',')
@@ -6,25 +9,37 @@ with open('results.csv') as file:
     for row in read:
         data.append(row)
 
-def test_avg(results, test, students):
+def test_avg(results, test):
     count = 0
     score = 0
-    for i in range(students):
+    for i in range(len(results)):
         score +=int(results[i][test])
         count +=1
-    return(score/count)
+    avg = score/count
+    list_average.append(avg)
+    return(avg)
 
-def high_score(results, test, students):
+def high_score(results, test):
     score = 0
-    for i in range(students):
+    for i in range(len(results)):
         if int((results[i][test])) > score:
             score = int((results[i][test]))
+    list_high.append(score)
     return(score)
 
-def low_score(results, test, students):
+def low_score(results, test):
     score = 100
-    for i in range(students):
+    for i in range(len(results)):
         if int((results[i][test])) < score:
             score = int((results[i][test]))
+    list_low.append(score)
     return(score)
+
+test_num=len(data[1])-1
+
+for i in range(test_num):
+    test_avg(data, i+1)
+    high_score(data, i+1)
+    low_score(data, i+1)
+
 
--- html_render.py
+++ html_render.py
@@ -1,21 +1,22 @@
 from jinja2 import Environment, FileSystemLoader
 import csv
-from calculations import high_score, data
+from calculations import list_low, list_high, list_average, data
 
 filename = 'output.html'
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("results.html")
 
-content = template.render(
-    max_score1=high_score(data, 1, 4),
-    max_score2=high_score(data, 2, 4),
-    max_score3=high_score(data, 3, 4)
-)
-
-
-with open(filename, mode="w", encoding="utf-8") as message:
-    message.write(content)
-    print(f"... wrote {filename}")
+results_filename = "output.html"
+results_template = environment.get_template("results.html")
+context = {
+    "tests": len(data),
+    "data_low": list_low,
+    "data_high": list_high,
+    "data_avg": list_average,
+}
+with open(results_filename, mode="w", encoding="utf-8") as results:
+    results.write(results_template.render(context))
+    print(f"... wrote {results_filename}")
 
 
 
